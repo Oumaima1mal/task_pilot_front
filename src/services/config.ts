1bfd47e04b23aba26f9453f55b7eed09a36fbd4a
Oumaima1mal <144392152+Oumaima1mal@users.noreply.github.com>
2025-06-11 20:03:38 +0100

    Update config.ts

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,6 @@
 // Configuration de l'API
 export const API_CONFIG = {
-    BASE_URL: "http://127.0.0.1:8000", // Assurez-vous que cette URL est correcte
+    BASE_URL: "https://task-pilot-back-production.up.railway.app", // Assurez-vous que cette URL est correcte
     HEADERS: {
       "Content-Type": "application/json",
     },
